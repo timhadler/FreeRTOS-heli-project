eca7a4034f9680378bd618b8e97c855c6c39eb59
Tim Hadler <tch118@canterbury.ac.nz>
2020-07-28 15:19:22 +1200

    Removed unnecessary global variable

--- heli_project/OLEDDisplay.h
+++ heli_project/OLEDDisplay.h
@@ -1,17 +1,18 @@
 /*
  * OLEDDisplay.h
  *
+ * Display header file for the Orbit OLED display used on the tm4c123gh6pm Tiva board
+ * Developed for University of Canterbury, ENCE464 Heli project
+ *
  *  Created on: 28/07/2020
- *      Author: tch118
+ *      Author: Tim Hadler
  */
 
 #ifndef OLEDDISPLAY_H_
 #define OLEDDISPLAY_H_
 
-//*****************************************************************************
-// Global Variables
-//*****************************************************************************
-char TEXT_BUFFER[16];       // Text buffer for the display, each line can hold 16 characters
+#include <stdint.h>
+
 
 //*****************************************************************************
 // Constants
@@ -23,11 +24,16 @@ enum displayLine {LINE_1 = 0, LINE_2, LINE_3, LINE_4};
 // Functions
 //*****************************************************************************
 
+// Initialise the Orbit OLED display
+void initDisplay(void);
+
 // Clears a line of the OLED display by writing a string of 16 spaces (" ") to it
-void ClearDisplayLine(displayLine line);
+void ClearDisplayLine(uint8_t line);
 
 // Writes a given string to the display on given line
-void writeDisplay(char text[], displayLine line);
+// Clears the line being written to first
+// Writes string starting at column = 0
+void writeDisplay(char text[16], uint8_t line);
 
 
 #endif /* OLEDDISPLAY_H_ */
